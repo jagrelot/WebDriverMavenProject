f8c5d63d7eda5d6f66788a1234d41aa62a047526
jagrelot <jagrelot@acumensolutions.com>
2017-11-02 14:45:13 -0400

    Add more test cases

--- src/test/java/manual_steps/Example.java
+++ src/test/java/manual_steps/Example.java
@@ -1,22 +1,36 @@
 package manual_steps;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class Example {
 
-	@Test
-	public void scriptMethod(){
+public WebDriver chromeDriver;
+	
+	@BeforeTest
+	public void beforeTestsetUp(){
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jagrelot\\Desktop\\chromedriver.exe");
+		chromeDriver = new ChromeDriver();
+	}
+	
+	@Test(priority=1,description="Verify Login")
+	public void loginTest(){
+		
 		WebElement  userName;
 		WebElement  passWord;
 		WebElement     login;
-		WebDriver chromeDriver = new ChromeDriver();
 		
 		chromeDriver.get("https://na59.salesforce.com");
 		userName = chromeDriver.findElement(By.cssSelector("#username"));
@@ -25,8 +39,36 @@ public class Example {
 		userName.sendKeys("jagrelot@orgdemo.com");
 		passWord.sendKeys("acumen321");
 		login.click();
-		chromeDriver.close();
+	
+	}
+	
+	@Test(priority=2, description="Create Accounts in Salesforce")
+	public void createAccounts(){
 		
+		WebElement    newBtn;
+		WebElement  acctName;
+		WebElement      save;
+		int 	   count = 0;
+		WebDriverWait wait = new WebDriverWait(chromeDriver, 10);
+		ArrayList<String> acctsToCreate = new ArrayList<String>();
+		acctsToCreate.add("Test Account TestNG - 1 - Jenkins JA");
+		//acctsToCreate.add("Test Account TestNG - 2 - Jenkins");
+	
+		for (String acct : acctsToCreate ) {
+				
+			wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#Account_Tab > a"))).click();
+			newBtn = chromeDriver.findElement(By.cssSelector("[name='new']"));
+			newBtn.click();
+			acctName = chromeDriver.findElement(By.cssSelector("#acc2"));
+			acctName.sendKeys(acct);
+			save = chromeDriver.findElement(By.cssSelector("td.pbButton > input:first-child"));
+			save.click();
+			count++;
+			System.out.println(acct + " was created Count:" + count);
+		}
+		
+		Assert.assertEquals(count, 2, "Error: The number of accounts created does not match");
+
 	}
 	
 }
